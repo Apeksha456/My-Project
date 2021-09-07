8758909cf03d5a740e551b9c2dcb6bc4ef9119b9
Apeksha456 <apekshamule96@gmail.com>
2021-09-06 21:31:59 -0700

    added a comment

--- boards/models.py
+++ boards/models.py
@@ -4,6 +4,8 @@ from django.utils.text import Truncator
 
 # Create your models here.
 
+# adding a new line in master branch
+
 class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
